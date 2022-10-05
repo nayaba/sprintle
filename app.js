625132860ce2be1099bc157fbf283f6cd2d32912
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-10-05 11:58:30 -0400

    Update and replace guesses with keypress rather than appending each guess

--- app.js
+++ app.js
@@ -3,28 +3,31 @@ let container = document.querySelector('.container')
 let guesses = ['', '', '', '', '']
 let counter = 0
 
+guesses.forEach((letter, i) => {
+  
+let alphaBox = document.createElement('div')
+alphaBox.classList.add('alpha-box')
+alphaBox.innerHTML = guesses[i]
+
+container.appendChild(alphaBox)
+  
+
+})
+
 const makeAlphaBoxes = (guess) => {
+  let alphaBox = document.querySelectorAll('.alpha-box')
 
   if(counter < 5) {
-
     guesses.splice(counter, 1, guess)
+    alphaBox[counter].innerHTML = guesses[counter]
     counter++
   } else {
     counter = 0
     guesses.splice(counter, 1, guess)
+    alphaBox[counter].innerHTML = guesses[counter]
     counter++
   }
   
-  guesses.forEach((letter, i) => {
-    
-  let alphaBox = document.createElement('div')
-  alphaBox.classList.add('alpha-box')
-  alphaBox.innerHTML = guesses[i]
-
-  container.appendChild(alphaBox)
-    
-
-  })
 
 }
 
