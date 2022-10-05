b420370a7a9b946f83adea87df8be644421412f8
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-10-05 11:04:55 -0400

    Splice letter guess into guesses array

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 let boxes = document.querySelectorAll('.box')
 let container = document.querySelector('.container')
-let guessArray = ['', '', '', '', '']
+let guesses = ['', '', '', '', '']
+let counter = 0
 
 const makeAlphaBoxes = (guess) => {
-  
-  guessArray.forEach((letter, i) => {
-    
-    // create alpha-box element
-    // element must be inside the loop and be created each time it is appended
-    // cannot be re-used
-    let alphaBox = document.createElement('div')
-    alphaBox.classList.add('alpha-box')
-    alphaBox.innerHTML = guess
 
-    // append to container
-    container.appendChild(alphaBox)
+  guesses.splice(counter, 1, guess)
+  counter++
+  
+  guesses.forEach((letter, i) => {
+    
+  let alphaBox = document.createElement('div')
+  alphaBox.classList.add('alpha-box')
+  alphaBox.innerHTML = guesses[i]
+
+  container.appendChild(alphaBox)
+    
 
   })
 
 }
-
 
 document.addEventListener('keydown',(e) => {
  makeAlphaBoxes(e.key.toUpperCase())
