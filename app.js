144b93d146ac97b0dc4d6fd0f553caf415f1f612
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-10-05 10:22:48 -0400

    Create square alpha boxes and accept keypress input

---
+++ app.js
@@ -0,0 +1,29 @@
+let boxes = document.querySelectorAll('.box')
+let container = document.querySelector('.container')
+let guessArray = ['', '', '', '', '']
+
+const makeAlphaBoxes = (guess) => {
+  
+  guessArray.forEach((letter, i) => {
+    
+    // create alpha-box element
+    // element must be inside the loop and be created each time it is appended
+    // cannot be re-used
+    let alphaBox = document.createElement('div')
+    alphaBox.classList.add('alpha-box')
+    alphaBox.innerHTML = guess
+
+    // append to container
+    container.appendChild(alphaBox)
+
+  })
+
+}
+
+
+document.addEventListener('keydown',(e) => {
+  let keyCode = e.keyCode
+  let chr = String.fromCharCode(keyCode)
+  console.log(chr)
+  makeAlphaBoxes(chr)
+} )
